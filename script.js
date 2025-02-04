d879f0ac251d670192976e3a398beb7768920d43
Jaime <jaimezpe.android@gmail.com>
2025-02-04 20:07:49 +0100

    alerta moviles

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+function esDispositivoMovil() {
+    return /Mobi|Android|iPhone|iPad|iPod|Tablet/i.test(navigator.userAgent);
+  }
 
+  if (esDispositivoMovil()) {
+    alert('lapaginaweb.com dice: Probablemente no se pueda ver bien la página web en este dispositivo.');
+  }
