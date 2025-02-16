b22601c9900dd77c56ffd04eb7267d392430d69b
Jaime <jaimezpe.android@gmail.com>
2025-02-16 20:11:43 +0100

    a

--- FixYourPC/script.js
+++
@@ -1,7 +0,0 @@
-function esDispositivoMovil() {
-    return /Mobi|Android|iPhone|iPad|iPod|Tablet/i.test(navigator.userAgent);
-  }
-
-  if (esDispositivoMovil()) {
-    alert('Probablemente no se pueda ver bien la página web en este dispositivo. Prueba a ponerlo en horizontal');
-  }
